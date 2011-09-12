c696685073669d3a26cc9933d0098492d90788ae
Karsten Thoms <karsten.thoms@itemis.de>
2011-09-12 13:04:23 +0200

    Avoiding cyclic resolution
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- plugins/org.xspray.xtext/src/org/xspray/xtext/customizing/XsprayQualifiedNameProvider.java
+++ plugins/org.xspray.xtext/src/org/xspray/xtext/customizing/XsprayQualifiedNameProvider.java
@@ -1,13 +1,20 @@
 package org.xspray.xtext.customizing;
 
+import java.util.List;
+
 import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
 import org.eclipse.xtext.naming.QualifiedName;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.xspray.mm.xspray.MetaAttribute;
 import org.xspray.mm.xspray.MetaClass;
+import org.xspray.mm.xspray.XsprayPackage;
 
 public class XsprayQualifiedNameProvider extends DefaultDeclarativeQualifiedNameProvider {
 	public QualifiedName qualifiedName (MetaClass element) {
-		return getFullyQualifiedName(element.getType());
+		List<INode> nodes = NodeModelUtils.findNodesForFeature(element, XsprayPackage.Literals.META_CLASS__TYPE);
+		String eClassName = NodeModelUtils.getTokenText(nodes.get(0));
+		return QualifiedName.create(element.getDiagram().getName(), eClassName);
 	}
 
 	/**
