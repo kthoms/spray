fe4daac3c7e6f47c6cb8738c2f41453007f5e242
Karsten Thoms <karsten.thoms@itemis.de>
2011-09-15 13:57:51 +0200

    Extended release script
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- releng/scripts/release.sh
+++ releng/scripts/release.sh
@@ -9,7 +9,7 @@ DEV_VERSION=$2
 DISTRO_DIR=$pwd/../../spray.distribution/release
 BASEDIR=$pwd
 
-
+pushd .
 
 if [ $# -ne $ARGCOUNT ]; 
 then
@@ -23,36 +23,55 @@ then
 	exit $ERRCODE 
 fi
 
-# set new version to release version
+echo "[spray-release] set new version to release version $REL_VERSION"
 mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION || exit
 
-# change category.xml afterwards since it is not replaced by tycho-versions-plugin
+echo "[spray-release] change category.xml afterwards since it is not replaced by tycho-versions-plugin"
 sed -i .bak 's/.qualifier//g'  repository/category.xml
 
-# execute Maven build
+echo "[spray-release] execute Maven build"
 mvn clean verify ||Êexit
 
-# rename target repository zip
+echo "[spray-release] rename target repository zip"
 mv repository/target/org.eclipselabs.spray.releng.repository.zip repository/target/spray-$REL_VERSION.zip 
 
-# Commit the changed files
+echo "[spray-release] commit the changed files"
 cd ..
 git commit -a -m "prepare for release"
 
-# Create a release tag with pattern v<version>
+echo "[spray-release] create release tag v$REL_VERSION"
 git tag v$REL_VERSION
 
 cd releng
 
-# Increment to next development version
+echo "[spray-release] Increment to next development version $DEV_VERSION"
 mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT ||Êexit
 
-# change the releng/repository/category.xml file
+echo "[spray-release] change the releng/repository/category.xml file"
 # important: usa double quotes here, since we refer to variables. single quote does not replace shell variables.
 sed -i .bak "s/$REL_VERSION/$DEV_VERSION\.qualifier/g" repository/category.xml
 
 #Execute a Maven build with goals clean verify to assure that everything builds 
 #mvn clean verify || exit
 
+echo "[spray-release] commit the changes" 
 cd ..
 git commit -a -m "increment to next development version"
+
+echo "[spray-release] push the changes including the tag to the server" 
+git --tags push origin master
+
+
+#
+echo "[spray-release] push Eclipse repository to spray.distribution"
+# Switch to the root directoy of spray.distribution
+cd $DISTRO_DIR
+echo "[spray-release] add, commit and push files in the spray.distribution repository"
+git add *
+git commit -m "releasing version $REL_VERSION"
+git push origin master
+
+popd
+
+echo "[spray-release] Process successfully completed."
+echo "[spray-release] Please upload $pwd/repository/target/spray-$REL_VERSION.zip to the project downloads http://code.google.com/a/eclipselabs.org/p/spray/downloads/list."
