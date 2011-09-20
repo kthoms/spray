f5d5f6850f5eededcf482e9b3015a7feeebf9236
Karsten Thoms <karsten.thoms@itemis.de>
2011-09-20 22:07:37 +0200

    Using Ecore as metamodel for unittests

--- tests/org.eclipselabs.spray.xtext.tests/src/org/eclipselabs/spray/xtext/tests/ModelTests.java
+++ tests/org.eclipselabs.spray.xtext.tests/src/org/eclipselabs/spray/xtext/tests/ModelTests.java
@@ -1,5 +1,6 @@
 package org.eclipselabs.spray.xtext.tests;
 
+import org.eclipse.emf.mwe.utils.StandaloneSetup;
 import org.eclipse.xtext.junit4.InjectWith;
 import org.eclipse.xtext.junit4.XtextRunner;
 import org.eclipselabs.spray.xtext.SprayInjectorProvider;
@@ -19,6 +20,10 @@ public class ModelTests extends XtextTest {
         // the first test case
         SprayStandaloneSetup.doSetup();
         suppressSerialization();
+        StandaloneSetup setup = new StandaloneSetup();
+        setup.setPlatformUri("../..");
+        setup.addRegisterGenModelFile("platform:/resource/org.eclipselabs.spray.xtext.tests/model/Ecore.genmodel");
+        setup.addRegisterEcoreFile("platform:/resource/org.eclipselabs.spray.xtext.tests/model/Ecore.ecore");
     }
 
     @Test
@@ -40,6 +45,6 @@ public class ModelTests extends XtextTest {
     }
     @Test
     public void test_31_expression_text() {
-        testFile("testcases/31-expression_text.spray", "mod4j/BusinessDomainDsl.ecore");
+        testFile("testcases/31-expression_text.spray", "Ecore.ecore");
     }
 }
