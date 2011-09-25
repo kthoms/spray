abb34ad0f1984c503b206f167310a4233825688d
Karsten Thoms <karsten.thoms@itemis.de>
2011-09-25 23:30:22 +0200

    renaming class

--- tests/org.eclipselabs.spray.examples.one.tests/src/org/eclipselabs/spray/examples/mod4j/features/Mod4jUpdateBusinessClassBusinessRulesFeatureTest.java
+++ tests/org.eclipselabs.spray.examples.one.tests/src/org/eclipselabs/spray/examples/mod4j/features/Mod4jUpdateBusinessClassBusinessRulesFeatureTest.java
@@ -1,5 +1,5 @@
 package org.eclipselabs.spray.examples.mod4j.features;
 
-public class Mod4jUpdateBusinessClassBusinessRulesFeatureTest2 {
+public class Mod4jUpdateBusinessClassBusinessRulesFeatureTest {
     private Mod4jUpdateBusinessClassBusinessRulesFeature fixture; 
 }
