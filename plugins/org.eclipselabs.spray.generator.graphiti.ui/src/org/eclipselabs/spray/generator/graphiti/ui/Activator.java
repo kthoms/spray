f483c58e1988ff94b4d7f83162b8b6ba0a0bb658
Karsten Thoms <karsten.thoms@itemis.de>
2011-09-30 22:04:02 +0200

    Changed order of modules
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- plugins/org.eclipselabs.spray.generator.graphiti.ui/src/org/eclipselabs/spray/generator/graphiti/ui/Activator.java
+++ plugins/org.eclipselabs.spray.generator.graphiti.ui/src/org/eclipselabs/spray/generator/graphiti/ui/Activator.java
@@ -16,33 +16,33 @@ import com.google.inject.Injector;
 /**
  */
 public class Activator extends AbstractUIPlugin {
-	private Injector injector;
-	private static Activator INSTANCE;
+    private Injector         injector;
+    private static Activator INSTANCE;
 
-	public Injector getInjector() {
-		return injector;
-	}
-	
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		INSTANCE = this;
-		try {
-		    injector = Guice.createInjector(Modules2.mixin(new SprayRuntimeModule(), new SharedStateModule(), new SprayUiModule(this), new GraphitiRuntimeModule(), new GraphitiGeneratorModule(), new GraphitiGeneratorUIModule(this)));
-		} catch (Exception e) {
-			Logger.getLogger(getClass()).error(e.getMessage(), e);
-			throw e;
-		}
-	}
-	
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		injector = null;
-		super.stop(context);
-	}
-	
-	public static Activator getInstance() {
-		return INSTANCE;
-	}
-	
+    public Injector getInjector() {
+        return injector;
+    }
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        INSTANCE = this;
+        try {
+            injector = Guice.createInjector(Modules2.mixin(new SprayRuntimeModule(), new GraphitiRuntimeModule(), new GraphitiGeneratorModule(), new GraphitiGeneratorUIModule(this), new SharedStateModule(), new SprayUiModule(this)));
+        } catch (Exception e) {
+            Logger.getLogger(getClass()).error(e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        injector = null;
+        super.stop(context);
+    }
+
+    public static Activator getInstance() {
+        return INSTANCE;
+    }
+
 }
