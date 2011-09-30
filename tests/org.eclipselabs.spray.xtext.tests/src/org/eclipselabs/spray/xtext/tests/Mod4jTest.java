d9c50c7610a39755461b3f8b5ba3327f0100ab57
Karsten Thoms <karsten.thoms@itemis.de>
2011-09-30 22:02:49 +0200

    upgrade to xtext-utils.unittesting 0.9.2
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- tests/org.eclipselabs.spray.xtext.tests/src/org/eclipselabs/spray/xtext/tests/Mod4jTest.java
+++ tests/org.eclipselabs.spray.xtext.tests/src/org/eclipselabs/spray/xtext/tests/Mod4jTest.java
@@ -5,9 +5,9 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.xtext.junit4.InjectWith;
-import org.eclipse.xtext.junit4.XtextRunner;
 import org.eclipselabs.spray.xtext.SprayTestsInjectorProvider;
 import org.eclipselabs.spray.xtext.SprayTestsStandaloneSetup;
+import org.eclipselabs.xtext.utils.unittesting.XtextRunner2;
 import org.eclipselabs.xtext.utils.unittesting.XtextTest;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -16,15 +16,12 @@ import org.junit.runner.RunWith;
 
 import BusinessDomainDsl.BusinessDomainDslPackage;
 
-@RunWith(XtextRunner.class)
+@RunWith(XtextRunner2.class)
 @InjectWith(SprayTestsInjectorProvider.class)
 public class Mod4jTest extends XtextTest {
     @Before
     public void before() {
         super.before();
-        // Normally, this should not be necessary, but the InjectorProvider only works for 
-        // the first test case
-        SprayTestsStandaloneSetup.doSetup();
         suppressSerialization();
         EPackage.Registry.INSTANCE.put(GenModelPackage.eNS_URI, GenModelPackage.eINSTANCE);
         EcorePlugin.getEPackageNsURIToGenModelLocationMap().put(BusinessDomainDslPackage.eNS_URI, URI.createURI("classpath:/mod4j/BusinessDomainDsl.genmodel"));
