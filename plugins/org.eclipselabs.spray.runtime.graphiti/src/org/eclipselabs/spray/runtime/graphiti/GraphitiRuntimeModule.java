4867f97bc34b98da995db531b7ba2f698b869378
Karsten Thoms <karsten.thoms@itemis.de>
2011-10-06 00:31:31 +0200

    Adding Graphiti default service bindings
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- plugins/org.eclipselabs.spray.runtime.graphiti/src/org/eclipselabs/spray/runtime/graphiti/GraphitiRuntimeModule.java
+++ plugins/org.eclipselabs.spray.runtime.graphiti/src/org/eclipselabs/spray/runtime/graphiti/GraphitiRuntimeModule.java
@@ -1,8 +1,18 @@
 package org.eclipselabs.spray.runtime.graphiti;
 
+import org.eclipse.graphiti.mm.pictograms.PictogramsFactory;
+import org.eclipse.graphiti.mm.pictograms.PictogramsPackage;
 import org.eclipse.graphiti.services.Graphiti;
+import org.eclipse.graphiti.services.ICreateService;
+import org.eclipse.graphiti.services.IGaCreateService;
+import org.eclipse.graphiti.services.IGaLayoutService;
 import org.eclipse.graphiti.services.IGaService;
+import org.eclipse.graphiti.services.ILayoutService;
+import org.eclipse.graphiti.services.ILinkService;
+import org.eclipse.graphiti.services.IMigrationService;
 import org.eclipse.graphiti.services.IPeCreateService;
+import org.eclipse.graphiti.services.IPeLayoutService;
+import org.eclipse.graphiti.services.IPeService;
 import org.eclipse.graphiti.util.IColorConstant;
 import org.eclipse.xtext.service.AbstractGenericModule;
 import org.eclipselabs.spray.xtext.api.IColorConstantTypeProvider;
@@ -24,16 +34,52 @@ public class GraphitiRuntimeModule extends AbstractGenericModule {
                 return new Class<?>[]{IColorConstant.class};
             }
         });
+
+        binder.bind(PictogramsPackage.class).toInstance(PictogramsPackage.eINSTANCE);
+        binder.bind(PictogramsFactory.class).toInstance(PictogramsFactory.eINSTANCE);
     }
 
     //---------------------------------------------------------------------------------------------
     // Bind Graphiti default services
     //---------------------------------------------------------------------------------------------
+    public void configureICreateService(Binder binder) {
+        binder.bind(ICreateService.class).toInstance(Graphiti.getCreateService());
+    }
+
+    public void configureIGaCreateService(Binder binder) {
+        binder.bind(IGaCreateService.class).toInstance(Graphiti.getGaCreateService());
+    }
+
     public void configureIGaService(Binder binder) {
         binder.bind(IGaService.class).toInstance(Graphiti.getGaService());
+    }
+
+    public void configureIGaLayoutService(Binder binder) {
+        binder.bind(IGaLayoutService.class).toInstance(Graphiti.getGaLayoutService());
+    }
+
+    public void configureILayoutService(Binder binder) {
+        binder.bind(ILayoutService.class).toInstance(Graphiti.getLayoutService());
+    }
+
+    public void configureILinkService(Binder binder) {
+        binder.bind(ILinkService.class).toInstance(Graphiti.getLinkService());
+    }
+
+    public void configureIMigrationService(Binder binder) {
+        binder.bind(IMigrationService.class).toInstance(Graphiti.getMigrationService());
     }
 
     public void configureIPeCreateService(Binder binder) {
         binder.bind(IPeCreateService.class).toInstance(Graphiti.getPeCreateService());
     }
+
+    public void configureIPeLayoutService(Binder binder) {
+        binder.bind(IPeLayoutService.class).toInstance(Graphiti.getPeLayoutService());
+    }
+
+    public void configureIPeService(Binder binder) {
+        binder.bind(IPeService.class).toInstance(Graphiti.getPeService());
+    }
+
 }
