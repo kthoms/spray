4117a41434f1cba33925706b1cf2294b54cba9e9
Karsten Thoms <karsten.thoms@itemis.de>
2011-11-01 18:51:27 +0100

    Release preparation
    set version must be called for each module now
    enhanced release script
    
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- releng/org.eclipselabs.spray.distribution/scripts/release.sh
+++ releng/org.eclipselabs.spray.distribution/scripts/release.sh
@@ -7,7 +7,7 @@ E_WRONGARGS=1
 REL_VERSION=$1
 DEV_VERSION=$2
 DISTRO_DIR=../../spray.distribution/releases
-BASEDIR=$pwd/../..
+BASEDIR=../..
 
 pushd .
 
@@ -24,7 +24,27 @@ then
 fi
 
 echo "[spray-release] set new version to release version $REL_VERSION"
+cd $BASEDIR/releng/org.eclipselabs.spray.distribution
 mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/releng/org.eclipselabs.spray.targetplatform/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/releng/org.eclipselabs.spray.parent/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/docs/org.eclipselabs.spray.doc.common/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/docs/org.eclipselabs.spray.doc.dev/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/docs/org.eclipselabs.spray.doc.user/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/plugins/org.eclipselabs.spray.generator.graphiti/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/plugins/org.eclipselabs.spray.generator.graphiti.ui/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/plugins/org.eclipselabs.spray.mm/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/plugins/org.eclipselabs.spray.runtime.graphiti/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/plugins/org.eclipselabs.spray.xtext/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/plugins/org.eclipselabs.spray.xtext.ui/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/examples/one/org.eclipselabs.spray.examples.one/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/tests/org.eclipselabs.spray.xtext.tests/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/tests/org.eclipselabs.spray.examples.one.tests/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/tests/org.eclipselabs.spray.generator.graphiti.tests/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/features/org.eclipselabs.spray.feature/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/features/org.eclipselabs.spray.feature.source/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/features/org.eclipselabs.spray.feature.sdk/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$REL_VERSION -f $BASEDIR/releng/org.eclipselabs.spray.repository/pom.xml || exit
 
 echo "[spray-release] change category.xml afterwards since it is not replaced by tycho-versions-plugin"
 sed -i .bak 's/.qualifier//g' ../org.eclipselabs.spray.repository/category.xml
@@ -46,6 +66,25 @@ git tag v$REL_VERSION
 
 echo "[spray-release] Increment to next development version $DEV_VERSION"
 mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/releng/org.eclipselabs.spray.targetplatform/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/releng/org.eclipselabs.spray.parent/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/docs/org.eclipselabs.spray.doc.common/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/docs/org.eclipselabs.spray.doc.dev/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/docs/org.eclipselabs.spray.doc.user/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/plugins/org.eclipselabs.spray.generator.graphiti/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/plugins/org.eclipselabs.spray.generator.graphiti.ui/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/plugins/org.eclipselabs.spray.mm/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/plugins/org.eclipselabs.spray.runtime.graphiti/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/plugins/org.eclipselabs.spray.xtext/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/plugins/org.eclipselabs.spray.xtext.ui/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/examples/one/org.eclipselabs.spray.examples.one/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/tests/org.eclipselabs.spray.xtext.tests/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/tests/org.eclipselabs.spray.examples.one.tests/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/tests/org.eclipselabs.spray.generator.graphiti.tests/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/features/org.eclipselabs.spray.feature/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/features/org.eclipselabs.spray.feature.source/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/features/org.eclipselabs.spray.feature.sdk/pom.xml || exit
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$DEV_VERSION-SNAPSHOT -f $BASEDIR/releng/org.eclipselabs.spray.repository/pom.xml || exit
 
 echo "[spray-release] change the releng/org.eclipselabs.spray.repository/category.xml file"
 # important: usa double quotes here, since we refer to variables. single quote does not replace shell variables.
