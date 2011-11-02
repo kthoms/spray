5ae1b795a353eb96a5b299d6d070415d652a2656
Karsten Thoms <karsten.thoms@itemis.de>
2011-11-02 23:56:01 +0100

    Open Java editor on activating extension file
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- plugins/org.eclipselabs.spray.xtext.ui/src/org/eclipselabs/spray/xtext/ui/commands/ActivateExtension.java
+++ plugins/org.eclipselabs.spray.xtext.ui/src/org/eclipselabs/spray/xtext/ui/commands/ActivateExtension.java
@@ -3,6 +3,7 @@ package org.eclipselabs.spray.xtext.ui.commands;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRoot;
@@ -14,7 +15,12 @@ import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.part.FileEditorInput;
 
 public class ActivateExtension extends AbstractHandler {
     private SprayJavaProjectUtil projectUtil = new SprayJavaProjectUtil();
@@ -38,6 +44,11 @@ public class ActivateExtension extends AbstractHandler {
                 }
                 cu.getResource().move(targetPath, true, new NullProgressMonitor());
                 root.findMember(targetPath).setDerived(false, new NullProgressMonitor());
+                IFile targetFile = (IFile) root.findMember(targetPath);
+                IEditorInput editorInput = new FileEditorInput(targetFile);
+                IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+                IWorkbenchPage page = window.getActivePage();
+                page.openEditor(editorInput, "org.eclipse.jdt.ui.CompilationUnitEditor");
                 //cu.move(srcFolder, null, null, false, new NullProgressMonitor());
             } catch (CoreException e) {
                 throw new ExecutionException(e.getMessage(), e);
